3e2993c1625d094cf7919ee61672e7a9fbae6d79
Mohan Vaddi <myselfgeek01@gmail.com>
2023-06-04 01:04:04 +0530

    feat: 🎸 added see_more in experience comp

--- src/_data/experience.ts
+++ src/_data/experience.ts
@@ -16,11 +16,11 @@ export default [
 		description: [
 			'Developing "[Sound Health](https://pointmotion.us/sound-health/)", a product that leverages motion-capture technology to analyze user\'s adherence and cognition by incorporating games and music to improve quality of life.',
 			'Responsible for building and maintaining APIs for Object overlay games, including APIs for Animations, Game logic, Collision detection and Music, contributing to seamless game-play experiences.',
+			'Worked on the back-end implementation of diverse systems, including authentication, analytics, and rewards systems.',
+			'Worked on planning and Implementation of the back-end infrastructure for Stripe payment systems for the entire platform that encompass various functionalities such as subscription plans, automated payments, and coupon systems.',
 			'Designed and developed a user-friendly mobile application that can be used as a remote control to manipulate the games, which helped in improving the efficiency and effectiveness of testing processes.',
 			'Contributed to the development of a chrome extension for user testers to easily record and share videos.',
 			'Contributed to the development of a desktop application to automate system accuracy testing and eliminated the need for manual testing efforts.',
-			'Worked on planning and Implementation of the back-end infrastructure for Stripe payment systems for the entire platform that encompass various functionalities such as subscription plans, automated payments, and coupon systems.',
-			'Worked on the back-end implementation of diverse systems, including authentication, analytics, and rewards systems.',
 			'Translated various product designs to responsive UI using Angular, Bootstrap, and SCSS on the front-end.',
 			'Created comprehensive technical documentation and videos outlining the functionality of select features.',
 			'Extensively collaborated with design, product, and business teams to understand their needs and develop successful features.',
--- src/lib/components/portfolio/Experience.svelte
+++ src/lib/components/portfolio/Experience.svelte
@@ -5,6 +5,20 @@
 	export let experience: Experience;
 
 	const { companyName, roles, description } = experience;
+
+	let customDescription: string[] = description;
+	let isExpanded: boolean;
+
+	// change this to increase/decrease the number of lines to show
+	// const maxDescriptionLength = 4;
+
+	// disabling the limit for the time being
+	const maxDescriptionLength = Infinity;
+
+	if (description.length > maxDescriptionLength) {
+		customDescription = description.slice(0, maxDescriptionLength);
+		isExpanded = false;
+	}
 </script>
 
 <div class="text-xl text-primary">{companyName}</div>
@@ -19,9 +33,25 @@
 <div class="description">
 	<div class="job-responsibilities mt-2 px-2">
 		<ul class="text-gray-400 text-md">
-			{#each description as line}
+			{#each customDescription as line}
 				<li>{@html insertHtml(line)}</li>
 			{/each}
 		</ul>
+
+		{#if description.length > maxDescriptionLength}
+			<button
+				on:click={() => {
+					customDescription = isExpanded ? description.slice(0, maxDescriptionLength) : description;
+					isExpanded = !isExpanded;
+				}}
+				class="text-primary text-md font-bold mt-2 underline"
+			>
+				{#if isExpanded}
+					See less
+				{:else}
+					See more
+				{/if}
+			</button>
+		{/if}
 	</div>
 </div>
